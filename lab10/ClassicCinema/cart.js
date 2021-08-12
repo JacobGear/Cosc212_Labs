a82102e0a963c0ec673a8221cb872e563c26ee71
JacobGear <imjacobgear@gmail.com>
2021-08-12 12:47:28 +1200

    Need help with Jquery :(

--- lab10/ClassicCinema/cart.js
+++ lab10/ClassicCinema/cart.js
@@ -24,9 +24,12 @@ let Cart = (function(){
     }
 
     function addToCart(){
-        let parent = this.parentNode.parentNode; //why double parent?
-        let price = parent.getElementsByClassName("price")[0].innerHTML;
-        let title = parent.getElementsByTagName("h3")[0].innerHTML;
+        //let parent = this.parentNode.parentNode;
+        let parent = $(this).parent().parent();
+        //let price = parent.getElementsByClassName("price")[0].innerHTML;
+        let price = $(parent).children(".price").html();
+        //let title = parent.getElementsByTagName("h3")[0].innerHTML;
+        let title = $(parent).children("h3").html();
         let item = {};
         item.price = price;
         item.title = title;
@@ -41,8 +44,9 @@ let Cart = (function(){
     pub.showCart = function () {
         let cart = getCart();
         let cartList = document.getElementById("cartList");
+        //let cartList = $("cartList");
 
-        if(cart == null){
+        if(cart === null){
             cartList.innerHTML = "<p> No items in cart </p>";
         } else {
             for(let item of cart){
@@ -52,6 +56,7 @@ let Cart = (function(){
         }
 
         let totalHTML = document.getElementById("total");
+        //let totalHTML = $("total");
         let total = 0.0;
         for(let item of cart){
             let p = parseFloat(item.price);
@@ -60,11 +65,13 @@ let Cart = (function(){
         totalHTML.innerHTML = "$" + total.toString();
 
         let clear = document.getElementById("clearCart");
+        //let clear = $("clearCart");
         clear.onclick = clearCart;
     }
 
     pub.showCheckout = function (){
         let id = document.getElementById("checkoutForm");
+        //let id = $("checkoutForm");
         let cart = getCart();
         if (cart === null){
             id.style.display = "none";
@@ -72,7 +79,8 @@ let Cart = (function(){
     }
 
     pub.setup = function(){
-        let btnElement = document.getElementsByClassName("buy");
+        //let btnElement = document.getElementsByClassName("buy");
+        let btnElement = $(".buy");
         for(let btn of btnElement){
             btn.onclick = addToCart;
         }
